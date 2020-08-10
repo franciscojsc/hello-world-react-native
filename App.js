62103603c66b1f3529007782efca7b49c814997d
Francisco Chaves <francisco.jschaves@gmail.com>
2020-08-10 14:49:24 -0300

    Add info about author

--- App.js
+++ App.js
@@ -6,6 +6,15 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World</Text>
+      <Image
+        style={styles.avatar}
+        source={{
+          uri:
+            'https://www.gravatar.com/avatar/405ac8b17609d976cf85971af1502666?s=200',
+        }}
+      ></Image>
+      <Text>My name is Francisco Chaves</Text>
+      <Text>GitHub: @franciscojsc</Text>
       <StatusBar style='auto' />
     </View>
   );
@@ -17,5 +26,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fcfcfc',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  avatar: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    margin: 10,
   },
 });
