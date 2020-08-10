7cb858220cb5301860bd210d2b6f84c7f65ba8af
Francisco Chaves <francisco.jschaves@gmail.com>
2020-08-10 14:19:20 -0300

    Alter text and color

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello World</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fcfcfc',
     alignItems: 'center',
     justifyContent: 'center',
   },
